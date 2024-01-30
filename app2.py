e3463a273022f14c1bee4eebd30853ef16acb890
KimEunSun <jianteow@gamil.com>
2024-01-30 13:25:34 +0900

    CREATED: new files

---
+++ app2.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import streamlit as st 
+import seaborn as sns 
+
+@st.cache_data
+def load_data():
+    df = sns.load_dataset("iris")
+    return df
+
+def main():
+    st.title("Hello World on Streamlit.io")
+    iris = load_data()
+    st.table(iris)
+
+if __name__ == "__main__":
+    main()
